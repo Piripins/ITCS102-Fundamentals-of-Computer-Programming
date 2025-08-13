5470ff9bcfaa0aea2a30d4eb384eca31f7a7b015
Piripins <denver16coc@gmail.com>
2025-08-13 16:03:17 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,20 @@
+deposit = input(" Enter amount to deposit --> ")
+a = deposit/1000  
+b = deposit/500
+c = deposit/200 
+d = deposit/100 
+e = deposit/50 
+f = deposit/20 
+g = deposit/10 
+h = deposit/5 
+i = deposit/1
+print(" Here is a breakdown of  the deposited amount: ")
+print(a, "1000")
+print(b, "500")
+print(c, "200")
+print(d, "100")
+print(e, "50")
+print(f, "20")
+print(g, "10")
+print(h, "5")
+print(i, "1")
