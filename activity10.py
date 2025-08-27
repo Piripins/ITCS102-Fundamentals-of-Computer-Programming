b902edb383ed677cd580a5d51f55ebaaca00678a
Piripins <denver16coc@gmail.com>
2025-08-27 15:09:26 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = input("Type your username --> ")
+password = input("Type your password --> ")
+
+uname = 'Denerbone'
+pword = 'brunominormyloves'
+
+if (uname == username) and (pword == password):
+	print("Login Succefully")
+else:
+	print("Login Failed")
