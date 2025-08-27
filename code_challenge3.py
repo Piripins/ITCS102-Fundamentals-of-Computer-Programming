21a0b65413e9c969afd1a5387f2404a9bc149af0
Piripins <denver16coc@gmail.com>
2025-08-27 20:51:30 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -2,14 +2,18 @@ name = input("What is your name? -> ")
 fare = eval(input("Please insert your fee -> "))
 isStudent = input("Are you a Student? -> ")
 
-if isStudent.lower() == 'Yes': 
+if isStudent.lower() == 'yes': 
 	print("Your amount will be discounted by 20 percent")
+	discount = fare * 0.2
+	new_fare = fare - discount 
+	sukli = fare - new_fare
+	print("Your fee", fare,  "has been discounted")
+	print("You have paid", new_fare)
+	print("Your change is", sukli)
 
-if isStudent == 'No' and isStudent == 'yes':
-	print("Your amount will not be discounted by 20 percent")
-
-if 'Yes' and 'yes':
-	print(
+else:
+	print("Sorry", name , "You will not be discounted by 20 percent")
+	print("You have paid", fare ,)
 
 
 
