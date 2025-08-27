d8e16342e0b6fdec08958c49cf7d54b7e33f9f14
Piripins <denver16coc@gmail.com>
2025-08-27 16:00:36 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,18 @@
+name = input("What is your name? -> ")
+fare = eval(input("Please insert your fee -> "))
+isStudent = input("Are you a Student? -> ")
+
+if isStudent.lower() == 'Yes': 
+	print("Your amount will be discounted by 20 percent")
+
+if isStudent == 'No' and isStudent == 'yes':
+	print("Your amount will not be discounted by 20 percent")
+
+if 'Yes' and 'yes':
+	print(
+
+
+
+
+
+	
