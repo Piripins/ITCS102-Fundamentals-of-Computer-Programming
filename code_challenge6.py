b9e5a13eacbe01959ad61bd20b1d8138ee9ade61
Piripins <denver16coc@gmail.com>
2025-09-15 19:45:58 +0800

    Create code_challenge6.py

---
+++ code_challenge6.py
@@ -0,0 +1,9 @@
+print("Welcome to the Code Challenge 6: Odd Number Summation")
+
+b1 = 0
+
+for i in range(7):
+    number = eval(input("Enter your number here --> "))
+    if number % 2 != 0:
+        b1 != number
+print("The summation of all odd numbers --> ", b1 )
