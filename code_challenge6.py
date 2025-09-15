3a7330c4d4978d40ae8fbc49baebc5a119c6714f
Piripins <denver16coc@gmail.com>
2025-09-15 19:48:00 +0800

    Update code_challenge6.py

--- code_challenge6.py
+++ code_challenge6.py
@@ -2,7 +2,7 @@ print("Welcome to the Code Challenge 6: Odd Number Summation")
 
 b1 = 0
 
-for i in range(7):
+for i in range(10):
     number = eval(input("Enter your number here --> "))
     if number % 2 != 0:
         b1 != number
