4e5b36fc70d78fee674ae67dd8bce3bf678fb6a8
Piripins <denver16coc@gmail.com>
2025-09-17 10:15:27 +0800

    Update code_challenge7.py

--- code_challenge7.py
+++ code_challenge7.py
@@ -1,1 +1,11 @@
+print("Welcome the Code Challenge 7: Project Liftoff")
 
+confirmation = input("Would like to start Project Liftoff (Yes/No) ")
+
+if confirmation == "yes":  
+    print("Countdown is starting")
+    for i in range(5, 0, -1):
+        print(f"{i}")
+    print("Liftoff!")
+else:
+    print("Okay, see you next time trooper!")
