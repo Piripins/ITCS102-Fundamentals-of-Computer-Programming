83665ae101a6b98c19e959817ba963c1ca9e7a25
Piripins <denver16coc@gmail.com>
2025-09-17 10:45:58 +0800

    Update code_challenge8.py

--- code_challenge8.py
+++ code_challenge8.py
@@ -1,1 +1,11 @@
+print("Welcome to the Code Challenge 8: Multiplication Table Maker")
 
+number = eval(input("Input the number you want to multiply --> "))
+
+if number > 0:
+    print('Multiplication table for', number)
+    for i in range(1, 11):
+        total = number * i
+        print(f"{number}X{i} =",total)
+else:
+    print("Invalid Input")
