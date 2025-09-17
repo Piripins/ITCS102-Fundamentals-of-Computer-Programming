134716ff3f38a0585d0150cd3b2e086f7e648a9e
Piripins <denver16coc@gmail.com>
2025-09-17 11:05:23 +0800

    Update code_challenge9.py

--- code_challenge9.py
+++ code_challenge9.py
@@ -1,6 +1,6 @@
 print("Welcome to the Code Challenge 9: 🦜 PARROT SIMULATOR – THE ECHO CHAMBER!")
 
-word = input('What would you like your parrot to say ')
+word = input('What would you like your parrot to say? ')
 number = eval(input("How many times should the parrot squawk it? "))
 
 for i in range(number, 0, -1):
