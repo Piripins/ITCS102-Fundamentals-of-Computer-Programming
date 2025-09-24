4a16fce58653260ccd9d20c0bc94f3f2eaac5bd0
Piripins <denver16coc@gmail.com>
2025-09-24 11:54:32 +0800

    Update activity18.py

--- activity18.py
+++ activity18.py
@@ -1,4 +1,6 @@
-for i in range(1, 11, 1):
-    for x in range(1, 11, 1):
-        print(x, end=" ")
+#Triangle Loop
+
+for i in range(1,11,1):
+    for x in range(1, i ,1):
+        print("*", end=" ")
     print()
