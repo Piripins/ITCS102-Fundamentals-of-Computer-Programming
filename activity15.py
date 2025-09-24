420213efdfd56a2e57151fa5abc8953821f67032
Piripins <denver16coc@gmail.com>
2025-09-24 11:52:36 +0800

    Update activity15.py

--- activity15.py
+++ activity15.py
@@ -1,4 +1,18 @@
+#String Formatting
+
 a = 'Hello'
 b = 'World'
 
+
 print(f"Did you want to say ( {a} {b} ) ?")
+
+
+
+number = 100
+
+print("Input Ten (10) numbers to subtract to the number 100")
+
+for i in range(1, 11, 1):
+    x = eval(input(f"{i} - Input the number here --> "))
+    number -= x
+print(f"This is the total ({number})")
