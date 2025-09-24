d0ddd11959682699ea3967b8c2f2877d7baec5ea
Piripins <denver16coc@gmail.com>
2025-09-24 11:52:53 +0800

    Update activity16.py

--- activity16.py
+++ activity16.py
@@ -1,8 +1,4 @@
-number = 100
-
-print("Input Ten (10) numbers to subtract to the number 100")
+#For Loop
 
 for i in range(1, 11, 1):
-    x = eval(input(f"{i} - Input the number here --> "))
-    number -= x
-print(f"This is the total ({number})")
+    print(i, end=" ")
