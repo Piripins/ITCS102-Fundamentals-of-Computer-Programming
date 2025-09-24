492a224f7c1c1d5fcc4d61bbb488f7689971720e
Piripins <denver16coc@gmail.com>
2025-09-24 11:53:57 +0800

    Update activity17.py

--- activity17.py
+++ activity17.py
@@ -1,8 +1,14 @@
 #Nested For Loop
 
+#part 1
 for i in range(1, 11, 1):
     for x in range(1, 11, 1):
         print(x, end=" ")
     print()
 
+#part 2
 
+for i in range(1,11,1):
+    for x in range(1, i ,1):
+        print(x, end=" ")
+    print(i)
