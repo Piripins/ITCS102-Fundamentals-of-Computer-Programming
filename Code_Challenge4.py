121687d7c2db5693eda49083dd28005bd7b4ae6d
Piripins <denver16coc@gmail.com>
2025-10-17 12:49:41 +0800

    Update Code_Challenge4.py

--- Code_Challenge4.py
+++ Code_Challenge4.py
@@ -1,70 +1,55 @@
 print("Welcome to Manhwa Recommender")
 
 manhwa = input("What Genre do you prefer? (Action/Regressor/Supernatural) ").lower()
-
 length = input("How long do you want to read? (Short/Medium/Long) ").lower()
-
 decade = input("What time of release would you like? (2000s/2010s) ")
 
-#Action
+# Action
 if manhwa == "action":
     if length == "short":
         if decade == "2000s":
             print("We recommend", "The Breaker")
+        elif decade == "2010s":
+            print("We recommend", "Revenge Of The Iron Blooded Sword Hound")
     elif length == "medium":
-        if decade == "2000s":
+        if decade == "2000s" or decade == "2010s":
             print("No Available Manhwa")
     elif length == "long":
         if decade == "2000s":
             print("We recommend", "The God of High School")
-    elif length == "short":
-        if decade== "2010s":
-            print("We recommend", "Revenge Of The Iron Blooded Sword Hound")
-    elif length == "medium":
-        if decade == "2010s":
-            print("No Available Manhwa")
-    elif length == "long":
-        if decade == "2010s":
+        elif decade == "2010s":
             print("We recommend", "Viral Hit")
-                
-#Regressor
-if manhwa == "regressor":
+
+# Regressor
+elif manhwa == "regressor":
     if length == "short":
         if decade == "2000s":
             print("No Available Manhwa")
-    elif length == "medium":
-        if decade == "2000s":
-            print("No Available Manhwa")
-    elif length == "long":
-        if decade == "2000s":
-            print("No Available Manhwa")
-    elif length == "short":
-        if decade== "2010s":
+        elif decade == "2010s":
             print("We recommend", "Absolute Necromancer")
     elif length == "medium":
-        if decade == "2010s":
+        if decade == "2000s":
+            print("No Available Manhwa")
+        elif decade == "2010s":
             print("We recommend", "Superhuman Battlefield")
     elif length == "long":
-        if decade == "2010s":
+        if decade == "2000s":
+            print("No Available Manhwa")
+        elif decade == "2010s":
             print("We recommend", "The Worn and Torn Newbie")
-    
-#Supernatural
-if manhwa == "supernatural":
+
+# Supernatural
+elif manhwa == "supernatural":
     if length == "short":
         if decade == "2000s":
-            print("We recommen", "BJ Archmage")
-    elif length == "medium":
-        if decade == "2000s":
-            print("No Available Manhwa")
-    elif length == "long":
-        if decade == "2000s":
-            print("No Available Manhwa")
-    elif length == "short":
-        if decade== "2010s":
+            print("We recommend", "BJ Archmage")
+        elif decade == "2010s":
             print("We recommend", "Questism")
     elif length == "medium":
-        if decade == "2010s":
-            print("We recommend", "Solo Leveling")
+        if decade == "2000s" or decade == "2010s":
+            print("No Available Manhwa")
     elif length == "long":
-        if decade == "2010s":
+        if decade == "2000s":
+            print("No Available Manhwa")
+        elif decade == "2010s":
             print("We recommend", "Nano Machine")
