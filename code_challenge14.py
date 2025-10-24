6a75bd0ed9634b0ec1ddcf72f67726262929b5d9
Piripins <denver16coc@gmail.com>
2025-10-24 21:09:45 +0800

    Update code_challenge14.py

--- code_challenge14.py
+++ code_challenge14.py
@@ -2,21 +2,24 @@ print('Welcome to your Anime Favorites Lister')
 
 name = input("What is your name? ")
 
-
 lahat = []
 
 while True: 
-    anime = input("Enter the title of the anime for your list of favotires (or type exit to finish) ").lower()
-    print(f" '{anime}' HAS BEEN ADDED TO YOUR LIST!!")
-    if anime == 'exit':
-        print("You've exited the program")
+    anime = input("Enter the title of the anime for your list of favotires (or type exit to finish): ").lower()
+    if anime != 'exit':
+        lahat.append(anime)
+        print(f" '{anime}' HAS BEEN ADDED TO YOUR LIST!!")
+    else:
+        print("You've exited the program")    
         break
-    lahat.append(anime)
 
 huwat = len(lahat)
-print(f"Hi {name}! You're anime list consist of:")
+print(f"Hi {name}! Your anime list consist of:")
 for i in range(1, huwat+1, 1):
     print(f'- {lahat[i-1]}')
     
+
+    
+
 
     
