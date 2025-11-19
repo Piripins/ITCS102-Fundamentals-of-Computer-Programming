3a59c46b8b90c77afc9398517c272d10cb0e9944
Piripins <denver16coc@gmail.com>
2025-11-19 15:50:47 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,11 @@
+prutas = {'Masarap kaso mabaho' : 'Durian', 
+          'Hindi masarap' : 'Bulok na mansanas', 
+          'Sakto lang' : 'Avocado na walang asukal', 
+          'Masarap talaga' : 'Binagkat na saging'}
+print(prutas)
+
+print(prutas['Masarap kaso mabaho'])
+
+prutas.pop('Hindi masarap')
+
+print(prutas)
