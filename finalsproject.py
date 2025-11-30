c2bbd5d26890e0a699f5230eb2c7dbb4f0114f08
Piripins <denver16coc@gmail.com>
2025-11-30 13:29:13 +0800

    Add files via upload

---
+++ finalsproject.py
@@ -0,0 +1,71 @@
+from submenu import *
+import os 
+import time, sys 
+
+os.system('cls')
+
+
+print('\nWelcome to LEARNING PYTHON')
+print('==========================')
+
+pangalan = input('What Is Your Name? ').upper()
+
+agreement = input(f'\nHello {pangalan}, Are You Ready To Learn Python? (Y/N) ').lower()
+
+if agreement == 'y':
+    for i in range(21):
+        bar = "█" * i + '-' * (20-i)
+        sys.stdout.write(f'\rLoading: [{bar}] {i*5}%')
+        sys.stdout.flush()
+        time.sleep(0.2)
+    os.system('cls')
+
+while True: 
+    if agreement == 'y':
+        menu()
+        choice = input('Enter The Number Of Your Chosen Topic--> ')
+            
+        if choice == '1':
+            printmenu()
+            continue
+        elif choice == '2':
+            variablesmenu()
+            continue
+        elif choice == '3':
+            operatorsmenu()
+            continue
+        elif choice == '4':
+            conditionalmenu()
+            continue
+        elif choice == '5':
+            loopmenu()
+            continue
+        elif choice == '6':
+            listmenu()
+            continue
+        elif choice == '7':
+            functionmenu()
+            continue
+        elif choice == '8':
+            sarilimenu()
+            continue
+        elif choice == '0': 
+            time.sleep(0.5)
+            print('\nThank You For Using This Program!')
+            time.sleep(0.5)
+            print('I Hope You Learned Something!')
+            time.sleep(0.5)
+            print('Exiting Program....')
+            time.sleep(0.5)
+            break
+        else:
+            print('Invalid Option Please Try Again')
+            continue
+    
+    elif agreement == 'n':
+        print(f'Thank You For Opening The Program, {pangalan}')
+        break
+        
+    
+    else: 
+        print('Invalid Input Please Try Again')
